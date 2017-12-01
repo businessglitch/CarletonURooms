0f0c20f7cdd2e56b0529eea6dc564ed1d60f4897
Fahad Hayat <hayat@pythian.com>
2017-11-30 20:27:57 -0500

    Added error handling in feedback component

--- src/app/components/tab-feedback/tab-feedback.component.ts
+++ src/app/components/tab-feedback/tab-feedback.component.ts
@@ -1,25 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, NgForm } from '@angular/forms';
+import { DataService } from '../../services/data.service';
+import { Response } from '../../models/Messages/response.interface';
+import { FeedbackForm } from '../../models/Forms/feedback.interface';
 
 @Component({
-  selector: 'tab-feedback',
-  templateUrl: './tab-feedback.component.html',
-  styleUrls: ['./tab-feedback.component.css']
+    selector: 'tab-feedback',
+    templateUrl: './tab-feedback.component.html',
+    styleUrls: ['./tab-feedback.component.css']
 })
 export class TabFeedbackComponent implements OnInit {
-	feedback = {
-    	firstName: '',
-    	lastName:  '',
-    	email: '',
-    	message: ''
-	};
-  	constructor() { }
+    response: Response;
+    feedback:FeedbackForm;
 
-	ngOnInit() {
-	}
+    constructor(private dataService:DataService) { 
+        this.feedback =  {
+            firstName: '',
+            lastName:  '',
+            email: '',
+            message: ''
+        };
 
-	onFormSubmit(feedbackForm: NgForm) {
-	  	$("#Feedback > div").addClass("loading");
-	}
+        this.response = {
+            data: '',
+            message:'',
+            status:''
+        };
+    }
+
+    ngOnInit() {}
+
+    onFormSubmit(feedbackForm: NgForm) {
+        $("#Feedback > div").addClass("loading");
+        if (feedbackForm.invalid) {
+            this.response.data = false;
+            this.response.message = "Form is Invalid!"
+            this.endLoading();
+        } else {
+            this.dataService
+                .sendMail(this.feedback)
+                .subscribe((res) => {
+                    this.response = res;
+                    this.endLoading();
+                },
+                err => console.error(err)
+                );
+        } 
+    }
+
+    endLoading() {
+        $("#Feedback > div").removeClass("loading");
+        $("#feedbackResponse").show();
+    }
 
 }
